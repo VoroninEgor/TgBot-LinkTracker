369f40f0af3754407280830633e3027cebf5d414
VoroninEgor <vegor4956@gmail.com>
2024-02-12 23:24:48 +0300

    feat: update BotApplication

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -1,14 +1,28 @@
 package edu.java.bot;
 
 import edu.java.bot.configuration.ApplicationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
-public class BotApplication {
+public class BotApplication implements CommandLineRunner {
+    private final MyTrackingBot bot;
+
+    @Autowired
+    public BotApplication(MyTrackingBot bot) {
+        this.bot = bot;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        bot.setUpdatesListener();
     }
 }
