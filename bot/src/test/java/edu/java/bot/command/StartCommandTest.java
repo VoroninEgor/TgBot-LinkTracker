3ead2b8ae083f94cd0a80abc3a64e32f500a892a
Voronin Egor <vegor4956@gmail.com>
2024-02-21 23:11:01 +0300

    test: add AbstractCommandTest

--- bot/src/test/java/edu/java/bot/command/StartCommandTest.java
+++ bot/src/test/java/edu/java/bot/command/StartCommandTest.java
@@ -1,31 +1,19 @@
 package edu.java.bot.command;
 
-import com.pengrad.telegrambot.model.Chat;
-import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import edu.java.bot.BotApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = BotApplication.class)
-class StartCommandTest {
+class StartCommandTest extends AbstractCommandTest {
 
     @Autowired
     StartCommand startCommand;
 
     @Test
     void handle() {
-        Update update = mock(Update.class);
-        Chat chat = mock(Chat.class);
-        when(chat.id()).thenReturn(5L);
-        Message message = mock(Message.class);
-        when(message.chat()).thenReturn(chat);
-        when(update.message()).thenReturn(message);
+        Update update = getMockUpdate(5L, "text");
 
         SendMessage sendMessage = startCommand.handle(update);
 
