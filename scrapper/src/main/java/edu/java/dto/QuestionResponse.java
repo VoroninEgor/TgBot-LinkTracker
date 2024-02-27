c77dde3346e090776a94a2f505057ec68465244d
Voronin Egor <vegor4956@gmail.com>
2024-02-27 13:04:43 +0300

    fix: rename QuestionItem to QuestionResponse

--- scrapper/src/main/java/edu/java/dto/QuestionResponse.java
+++ scrapper/src/main/java/edu/java/dto/QuestionResponse.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
 import java.util.List;
 
-public record QuestionItem(List<QuestionResponse> items) {
-    public record QuestionResponse(
+public record QuestionResponse(List<QuestionItem> items) {
+    public record QuestionItem(
         @JsonProperty("question_id")
         Long id,
         @JsonProperty("last_activity_date")
