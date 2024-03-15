8f1223f7097f4bda0f59ba05c398c26280f2d3e0
Voronin Egor <vegor4956@gmail.com>
2024-03-15 21:51:58 +0300

    feat: update LinkUpdaterScheduler to check update for links

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -1,16 +1,69 @@
 package edu.java.scheduler;
 
+import edu.java.client.bot.BotClient;
+import edu.java.dto.LinkUpdateRequest;
+import edu.java.dto.link.LinkUpdateResponse;
+import edu.java.service.LinkService;
+import edu.java.service.TgChatService;
+import edu.java.util.LinkUpdateChecker;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import static java.lang.String.format;
 
 @Component
 @EnableScheduling
+@RequiredArgsConstructor
 @Slf4j
 public class LinkUpdaterScheduler {
+    private final LinkService linkService;
+    private final TgChatService tgChatService;
+    private final LinkUpdateChecker linkUpdateChecker;
+    private final BotClient botClient;
+    private final Long forceCheckDelay;
+
     @Scheduled(fixedDelayString = "#{@schedulerIntervalMs}")
     public void update() {
         log.info("LinkUpdaterScheduler hase seen update");
+
+        List<LinkUpdateResponse> linksToCheckForUpdates = linkService.findLinksToCheckForUpdates(forceCheckDelay);
+        List<LinkUpdateRequest> linksToUpdate = getLinksToUpdate(linksToCheckForUpdates);
+
+        log.info("Updated links: " + linksToUpdate);
+
+        linksToUpdate.forEach(botClient::updatesPost);
+    }
+
+    private List<LinkUpdateRequest> getLinksToUpdate(List<LinkUpdateResponse> linksToCheckForUpdates) {
+        List<LinkUpdateRequest> linksToUpdate = new ArrayList<>();
+
+        for (LinkUpdateResponse linkToCheck : linksToCheckForUpdates) {
+            OffsetDateTime oldUpdateTime = linkToCheck.updatedAt();
+            OffsetDateTime lastUpdateTime = linkUpdateChecker.getLastUpdateTime(linkToCheck.url().toString());
+            if (lastUpdateTime == null) {
+                log.warn("link not found update");
+
+                linkService.remove(linkToCheck.url());
+            } else if (oldUpdateTime == null || oldUpdateTime.isBefore(lastUpdateTime)) {
+                log.info(format("old update time: %s | new update time: %s", oldUpdateTime, lastUpdateTime));
+
+                List<Long> tgChatsId = tgChatService.fetchTgChatsIdByLink(linkToCheck.url());
+                LinkUpdateRequest linkToUpdate = LinkUpdateRequest.builder()
+                    .id(linkToCheck.id())
+                    .url(linkToCheck.url())
+                    .tgChatIds(tgChatsId)
+                    .description("There is update for " + linkToCheck.url())
+                    .build();
+                linksToUpdate.add(linkToUpdate);
+            }
+            linkService.updateLink(linkToCheck.url(), lastUpdateTime);
+        }
+
+        return linksToUpdate;
     }
 }
---
+++ scrapper/src/main/java/edu/java/util/UrlParser.java
@@ -0,0 +1,39 @@
+package edu.java.util;
+
+import edu.java.exception.UrlParseException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UrlParser {
+    private static final String GITHUBLINK = "^https://github\\.com/[^/]+/[^/]+.*$";
+    private static final String STACKOVERFLOWLINK = "^https://stackoverflow\\.com/questions/\\d+/.*$";
+
+    public String fetchRepoNameFromGitHubLink(String url) {
+        if (!url.matches(GITHUBLINK)) {
+            throw new UrlParseException();
+        }
+        int startIndex = url.lastIndexOf("/") + 1;
+        return url.substring(startIndex);
+    }
+
+    public String fetchUserNameFromGitHubLink(String url) {
+        if (!url.matches(GITHUBLINK)) {
+            throw new UrlParseException();
+        }
+        int distanceToStartName = 5;
+        int startIndex = url.indexOf(".com/") + distanceToStartName;
+        int endIndex = url.lastIndexOf("/");
+        return url.substring(startIndex, endIndex);
+    }
+
+    public Long fetchQuestionIdFromStackOverFlowLink(String url) {
+        if (!url.matches(STACKOVERFLOWLINK)) {
+            throw new UrlParseException();
+        }
+        int distanceToId = 10;
+        int startIndex = url.indexOf("questions/") + distanceToId;
+        int endIndex = url.lastIndexOf("/");
+        String id = url.substring(startIndex, endIndex);
+        return Long.parseLong(id);
+    }
+}
